c861902e0aac8dff3a75ae60ebf85fceff580003
dtjong <davintjong@gmail.com>
2021-02-08 16:00:39 -0800

    Added constraint semantics

--- src/constraint_solver.py
+++ src/constraint_solver.py
@@ -1,29 +1,36 @@
 from z3 import *
-from enum import Enum
+from enum import IntEnum
 
-class ViewType(Enum):
-    VStack = 1
-    HStack = 2
-    Leaf = 3
+class ViewType(IntEnum):
+    VStack = 0
+    HStack = 1
+    Leaf = 2
 
 class View:
     def __init__(self, top_left, bot_right, parent=None, view_type=ViewType.Leaf):
         self.top_left = top_left
         self.bot_right = bot_right
         self.parent = parent
+        self.view_type = view_type
 
         # Constraints for H/VStack
         self.spacing_constraint = 0
         # Constraints for leaf views
-        self.padding_constraint = None
+        self.padding_constraint = [[0, 0], [0, 0]]
         self.frame_constraint = None
 
     def __eq__(self, other):
         return self.top_left == other.top_left and self.bot_right == other.bot_right
 
-    # 0 for width, 1 for height
+    def __str__(self):
+        return str(self.top_left) + ", " + str(self.bot_right)
+
+    # 0 for height, 1 for width
     def size(self, axis):
-        return bot_right[axis] - top_left[axis]
+        return self.bot_right[axis] - self.top_left[axis]
+
+    def is_framed(self, axis):
+        return self.frame_constraint != None and self.frame_constraint[axis] != None
 
     # Below are functions to generate the constraints
     def gen_padding(self, top, bot=None, left=None, right=None):
@@ -33,27 +40,60 @@ class View:
             left = top
         if right == None:
             right = left
-        self.padding_constraint = {
-            "top": top,
-            "bot": bot,
-            "left": left,
-            "right": top,
-        }
-
+        self.padding_constraint = [[top, bot], [left, right]]
     def gen_spacing(self, spacing):
         self.spacing_constraint = spacing
 
-    def gen_frame(self, width=None, height=None):
-        self.frame_constraint = {
-            "width": width,
-            "height": height
-        }
+    def gen_frame(self, height=None, width=None):
+        self.frame_constraint = [height, width]
 
+# Constraint Solver for one level (no hierarchy)
 class ConstraintSolver:
     def __init__(self, views):
         self.s = Solver()
         self.views = views
 
+    def verify(self):
+        constrained_views = self.constraint_to_coords()
+        return constrained_views == self.views
+
     # Outputs a list of views in the same format as the input
     def constraint_to_coords(self):
-        num_frameless = len(filter(lambda view: view.frame_constraint == None, self.views))
+        if len(self.views) == 0:
+            return
+        root = self.views[0]
+        major_axis = int(root.view_type)
+        minor_axis = int(ViewType.HStack if root.view_type == ViewType.VStack else ViewType.VStack)
+        children = self.views[1:]
+        # Calculating width/height of frameless views
+        spacing = (len(children) - 1) * root.spacing_constraint
+        framed = filter(lambda view: view.is_framed(major_axis), children)
+        num_frameless = len(list(filter(lambda view: not view.is_framed(major_axis), children)))
+        pad_sum = sum([sum(view.padding_constraint[major_axis])
+                       for view in children])
+        framed_sum = sum([view.frame_constraint[major_axis]
+                          for view in framed])
+        size = root.size(major_axis) # size along major axis
+        fsize = (size - spacing - pad_sum - framed_sum) / num_frameless if num_frameless > 0 else 0
+        if fsize < 0:
+            return
+        last = 0
+        result = [root]
+        for view in children:
+            top_left = root.top_left.copy()
+            last += view.padding_constraint[major_axis][0]
+            top_left[major_axis] += last
+            top_left[minor_axis] += view.padding_constraint[minor_axis][0]
+            bot_right = top_left.copy()
+            if view.is_framed(major_axis):
+                bot_right[major_axis] += view.frame_constraint[major_axis]
+            else:
+                bot_right[major_axis] += fsize
+            last = bot_right[major_axis] + view.padding_constraint[major_axis][1] + root.spacing_constraint
+
+            if view.is_framed(minor_axis):
+                bot_right[minor_axis] += view.frame_constraint[minor_axis]
+            else:
+                bot_right[minor_axis] = root.bot_right[minor_axis] - view.padding_constraint[minor_axis][1]
+            result.append(View(top_left, bot_right))
+        return result
--- src/test_constraint_solver.py
+++ src/test_constraint_solver.py
@@ -1,18 +1,61 @@
 import unittest
-from .constraint_solver import *
+from constraint_solver import *
 
 class TestSolver(unittest.TestCase):
-    def test_padding_no_frame_no_spacing(self):
-        root = View((0, 0), (100, 100))
-        child1 = View((10, 10), (90, 40), root)
-        child2 = View((10, 60), (90, 90), root)
+    pass
+
+# Testing constraint -> coord math
+class TestConstraints(unittest.TestCase):
+    def test_padding_vstack(self):
+        root = View([0, 0], [100, 100], view_type=ViewType.VStack)
+        child1 = View([10, 10], [40, 90], root)
+        child2 = View([60, 10], [90, 90], root)
 
         child1.gen_padding(10)
         child2.gen_padding(10)
 
         view_list = [root, child1, child2]
         views_output = ConstraintSolver(view_list).constraint_to_coords()
-        self.assertTrue(view_list = views_output)
+        self.assertListEqual(view_list, views_output)
+
+    def test_padding_hstack(self):
+        root = View([0, 0], [100, 100], view_type=ViewType.HStack)
+        child1 = View([10, 10], [90, 40], root)
+        child2 = View([10, 60], [90, 90], root)
+
+        child1.gen_padding(10)
+        child2.gen_padding(10)
+
+        view_list = [root, child1, child2]
+        views_output = ConstraintSolver(view_list).constraint_to_coords()
+        self.assertListEqual(view_list, views_output)
+
+    def test_frame_and_spacing(self):
+        root = View([0, 0], [100, 100], view_type=ViewType.HStack)
+        child1 = View([0, 0], [30, 30], root)
+        child2 = View([0, 45], [50, 50], root)
+        child3 = View([0, 65], [30, 90], root)
+
+        root.gen_spacing(15)
+        child1.gen_frame(30, 30)
+        child2.gen_frame(50, 5)
+        child3.gen_frame(30, 25)
+
+        view_list = [root, child1, child2, child3]
+        views_output = ConstraintSolver(view_list).constraint_to_coords()
+        self.assertListEqual(view_list, views_output)
+
+    def test_spacing(self):
+        root = View([0, 0], [100, 100], view_type=ViewType.HStack)
+        child1 = View([0, 0], [100, 20], root)
+        child2 = View([0, 80], [100, 100], root)
+
+        root.gen_spacing(60)
+
+        view_list = [root, child1, child2]
+        views_output = ConstraintSolver(view_list).constraint_to_coords()
+        self.assertListEqual(view_list, views_output)
+
 
 if __name__ == '__main__':
     unittest.main()
